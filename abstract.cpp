8bec0afc6517fd4be2a95eff6829ca360b32d1c9
SathvikShetty1 <119489811+SathvikShetty1@users.noreply.github.com>
2023-03-09 23:21:49 +0530

    Add files via upload

---
+++ abstract.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+class impAb {
+private:
+	int a, b;
+
+public:
+	void set(int x, int y)
+	{
+		a = x;
+		b = y;
+	}
+
+	void display()
+	{
+		cout << "a = " << a << endl;
+		cout << "b = " << b << endl;
+	}
+};
+
+int main()
+{
+	impAb obj;
+	obj.set(10, 20);
+	obj.display();
+	return 0;
+}
